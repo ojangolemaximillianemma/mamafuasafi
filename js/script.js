d029903ff4b0f4ab3e36caa86f672b855a81cf13
ojangolemaximillianemma <ojangolemax@gmail.com>
2026-05-20 19:43:23 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
 
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
